eede23b8e144ecd15af4b48dc3a6fe221d834119
m-szymborska <monika_szymborska@tlen.pl>
2023-03-24 12:49:33 +0100

    7projektTodoList

---
+++ 7projekt.js
@@ -0,0 +1,114 @@
+let todoInput
+let errorInfo
+let addBtn
+let ulList
+let newTodo
+
+let popup
+let popupInfo
+let todoToEdit
+let popupInput
+let popupAddBtn
+let popupCloseBtn
+
+const main = () => {
+	prepareDOMElements()
+	prepareDOMEvents()
+}
+
+const prepareDOMElements = () => {
+	todoInput = document.querySelector('.todo-input')
+	errorInfo = document.querySelector('.error-info')
+	addBtn = document.querySelector('.btn-add')
+	ulList = document.querySelector('.todolist ul')
+
+	popup = document.querySelector('.popup')
+	popupInfo = document.querySelector('.popup-info')
+	popupInput = document.querySelector('.popup-input')
+	popupAddBtn = document.querySelector('.accept')
+	popupCloseBtn = document.querySelector('.cancel')
+}
+
+const prepareDOMEvents = () => {
+	addBtn.addEventListener('click', addNewTodo)
+	ulList.addEventListener('click', checkClick)
+	popupCloseBtn.addEventListener('click', closePopup)
+	popupAddBtn.addEventListener('click', changeTodoText)
+	todoInput.addEventListener('keyup', enterKeyCheck)
+}
+
+const addNewTodo = () => {
+	if (todoInput.value !== '') {
+		newTodo = document.createElement('li')
+		newTodo.textContent = todoInput.value
+		createToolsArea()
+		ulList.append(newTodo)
+		todoInput.value = ''
+		errorInfo.textContent = ''
+	} else {
+		errorInfo.textContent = 'Wpisz treść zadania'
+	}
+}
+
+const createToolsArea = () => {
+	const toolsPanel = document.createElement('div')
+	toolsPanel.classList.add('tools')
+	newTodo.append(toolsPanel)
+
+	const completeBtn = document.createElement('button')
+	completeBtn.classList.add('complete')
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+	const editBtn = document.createElement('button')
+	editBtn.classList.add('edit')
+	editBtn.textContent = 'EDIT'
+
+	const deletBtn = document.createElement('button')
+	deletBtn.classList.add('delete')
+	deletBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+	toolsPanel.append(completeBtn, editBtn, deletBtn)
+}
+
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed')
+		e.target.classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		editTodo(e)
+	} else if (e.target.matches('.delete')) {
+		deleteTodo(e)
+	}
+}
+
+const editTodo = e => {
+	todoToEdit = e.target.closest('li')
+	popupInput.value = todoToEdit.firstChild.textContent
+	console.log(todoToEdit.firstChild)
+	popup.style.display = 'flex'
+}
+const closePopup = () => {
+	popup.style.display = 'none'
+	popupInfo.textContent = 'none'
+}
+
+const changeTodoText = () => {
+	if (popupInput.value !== '') {
+		todoToEdit.firstChild.textContent = popupInput.value
+		popup.style.display = 'none'
+		popupInfo.textContent = ''
+	} else {
+		popupInfo.textContent = 'Musisz podać jakąś treść'
+	}
+}
+const deleteTodo = e => {
+	e.target.closest('li').remove()
+	const allTodos = ulList.querySelectorAll('li')
+	if (allTodos.length === 0) errorInfo.textContent = 'Brak zadań na liście'
+}
+const enterKeyCheck = e => {
+	if (e.key === 'Enter') {
+		addNewTodo()
+	}
+}
+document.addEventListener('DOMContentLoaded', main)
